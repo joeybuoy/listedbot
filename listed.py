aa0e69ad14e6e178e174ceac4db2845a31dbcba8
joeybuoy <146126943+joeybuoy@users.noreply.github.com>
2023-09-28 18:10:33 -0400

    Add files via upload

--- listed.py
+++ listed.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #Discord Bot listed.py
 # using packages discord.py & python-dotenv
 
@@ -6,6 +7,7 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import sqlite3
 
 #assign token
 load_dotenv()
@@ -22,6 +24,8 @@ bot = commands.Bot(command_prefix='/', description=description, intents=intents)
 
 #example prompt /add coraline to $movielist
 #initialize database
+con = sqlite3.connect("listedbot.db")
+cur = con.cursor()
 
 #adding to list
 @bot.command()
@@ -30,24 +34,24 @@ async def add(ctx,arg, case_insensitive = True):
     splitString = arg.split("$")
     frontmsg= splitString[1]
     ideaname= frontmsg.pop(-1)
-    listname.tlt= splitString[2]
+    listname_tlt= splitString[2]
+    
+    # find and add to database
     try:
-       # pull from database
+     result = cur.execute("""
+    INSERT INTO ? VALUES (?) """, (listname.tlt,ideaname))
+
+   #ctx.send(f'{ideaname} added to {listname\_tlt}!')
+    
     except Exception:
-       #search database for match
+       result.fetchone() is None
        await ctx.send(f"{listname_tlt} doesn't exist :< !")
        return
-   #append new idea to existing list
-    listname= [listname;ideaname];
-   
-          ctx.send(f"{ideaname} added to {listname.tlt}!")
-       
-
 
 #removing from list (by number) /remove $movielist 3
+#also used to remove lists themselves
 @bot.command()
-    
-    async def remove(ctx,arg,int, case_insensitive=True):
+async def remove(ctx,arg,int, case_insensitive=True):
        removal_int= int
      #  if "-" in (message.content.lower()).replace(" ", ""):
          
@@ -56,8 +60,8 @@ async def add(ctx,arg, case_insensitive = True):
 #same as removal except pop is a move if archive x doesnt exist, create it
 #renaming list /rename
 
-@bot.command()
-async def rename(ctx,arg):
+#@bot.command()
+#async def rename(ctx,arg):
        #remove $ from movielist
        #search for it in database
        #rename key
@@ -68,14 +72,23 @@ async def view(ctx, arg)
 msg= str(arg)
         listname= msg[1:]
               # search in database for match
-              for number, listname in enumerate(listname):
-              await #print list header
-                     print(number, listname)
+             
+              try:
+            result= res.fetchall("SELECT idea name FROM ?",(arg))
+              for number, result in enumerate(result):
+              await print f"{arg}"
+                     print(number, result)
+              except Exception:
+                 result.fetchall() is None
+                 await ctx.send(f"{listname_tlt} doesn't exist :< !")
+                 return
+              
                      
 #create list
 @bot.command()
 async def create(ctx,*,arg):
     #enter new list into database
-  total_lists=[total_lists], str(arg)]
+    tb_create = f"CREATE TABLE [{arg}](idea name)"
+    conn.execute(tb_create)
     await ctx.send(f"{arg} added to lists!")
         
